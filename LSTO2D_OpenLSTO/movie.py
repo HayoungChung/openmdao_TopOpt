9d55233d65821a12586aa6ad757f1ea37a0954a5
Hayoung Chung <hac210@eng.ucsd.edu>
2018-10-25 13:54:25 -0700

    rename LSTO

--- LSTO2D_OpenLSTO/movie.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-
-cmd = 'ffmpeg -i save/save%03d.png -vcodec mpeg4 save/test.avi'
-call(cmd.split())
-cmd = 'ffmpeg -i save/test.avi -acodec libmp3lame -ab 192 save/test.mov'
-call(cmd.split())
